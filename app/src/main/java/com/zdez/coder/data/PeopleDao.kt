097d967ce4ca94cfa77e5e9fffe683db3fd9a0ec
zdezak <zdezarus@gmail.com>
2022-01-16 00:16:30 +0500

    create classes for database

---
+++ app/src/main/java/com/zdez/coder/data/PeopleDao.kt
@@ -0,0 +1,23 @@
+package com.zdez.coder.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface PeopleDao {
+
+    @Insert
+    suspend fun insertPeople(people: List<People>)
+
+    @Update
+    suspend fun update(people: List<People>)
+
+    @Query("SELECT * FROM people ORDER BY :order DESC")
+    suspend fun getAllPeople(order: String): List<People>
+
+    @Query("SELECT * FROM people WHERE department = :key ORDER BY :order DESC")
+    suspend fun getAllPeopleInDepartment(key: String, order: String): List<People>
+
+}
---
+++ app/src/main/java/com/zdez/coder/data/PeopleDatabase.kt
@@ -0,0 +1,33 @@
+package com.zdez.coder.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [People::class], version = 1, exportSchema = false)
+abstract class PeopleDatabase : RoomDatabase() {
+    abstract val peopleDao: PeopleDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: PeopleDatabase? = null
+        fun getInstance(context: Context): PeopleDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        PeopleDatabase::class.java,
+                        "people"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                // Return instance; smart cast to be non-null.
+                return instance
+            }
+        }
+    }
+}
