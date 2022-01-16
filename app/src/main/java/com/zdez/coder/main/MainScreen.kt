8058968b0903dc79ea73f95fe250739899c97de7
zdezak <zdezarus@gmail.com>
2022-01-16 14:37:15 +0500

    add TopAppBar and Tabs

--- app/src/main/java/com/zdez/coder/main/MainScreen.kt
+++ app/src/main/java/com/zdez/coder/main/MainScreen.kt
@@ -1,35 +1,84 @@
 package com.zdez.coder.main
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun MainScreen(navController: NavController) {
-
-    var textForSearch by remember {mutableStateOf("")    }
+    val expanded = remember { mutableStateOf(false) }
+    val order = remember { mutableStateOf("firstName") }
+    var textForSearch by remember { mutableStateOf("") }
+    val selectedTabIndex = remember { mutableStateOf(0)}
+    val tabs = listOf<String>(
+        "all",
+        "android",
+        "ios",
+        "design",
+        "management",
+        "qa",
+        "back_office",
+        "frontend",
+        "hr",
+        "pr",
+        "backend",
+        "support",
+        "analytics"
+    )
 
     Scaffold(topBar = {
         TopAppBar(
             title = {
                 TextField(
                     value = textForSearch,
-                    onValueChange = { textForSearch = it })
+                    onValueChange = { textForSearch = it },
+                    modifier = Modifier.fillMaxWidth(1f),
+                    placeholder = { Text(text = "Введите имя, фамилию или тег")}
+                )
             },
             navigationIcon = {
-                Icon(Icons.Filled.Search, contentDescription = "Search textField")
+                Icon(
+                    Icons.Filled.Search,
+                    contentDescription = "Search textField",
+                    modifier = Modifier
+                        .wrapContentSize(Alignment.TopStart)
+                        .fillMaxWidth(0.90f))
+            },
+            actions = {
+                Box(modifier = Modifier.wrapContentSize(Alignment.TopEnd)) {
+                    IconButton(onClick = { expanded.value = true }) {
+                        Icon(Icons.Filled.Menu, contentDescription = "Sorting")
+                    }
+                    DropdownMenu(expanded = expanded.value,
+                        onDismissRequest = { expanded.value = false }) {
+                        DropdownMenuItem(onClick = {
+                            expanded.value = false
+                            order.value = "firstName"
+                        }) {
+                            Text(text = "По алфавиту")
+                        }
+                        DropdownMenuItem(onClick = {
+                            expanded.value = false
+                            order.value = "birthday"
+
+                        }) {
+                            Text(text = "По дате рождения")
+                        }
+                    }
+                }
             }
-            //TODO Actions
         )
     }
     ) {
@@ -38,6 +87,16 @@ fun MainScreen(navController: NavController) {
                 .fillMaxHeight()
         ) {
             //TODO Tabs
+            ScrollableTabRow(selectedTabIndex =selectedTabIndex.value) {
+                tabs.forEachIndexed(){index, tab->
+                    Tab(
+                        selected = index == selectedTabIndex.value,
+                        onClick = { selectedTabIndex.value = index},
+                    text = {Text(text = tab)}
+                    )
+                }
+            }
+
             LazyColumn(verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.Start
             ) {
