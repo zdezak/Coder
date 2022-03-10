dd89cd871b802fbac33917bddeb7149a1096bc69
zdezak <zdezarus@gmail.com>
2022-03-10 15:58:33 +0500

    add LoadingScreenTest

---
+++ app/src/androidTest/java/com/zdez/coder/feature_user_list/presentation/loading/LoadingScreenTest.kt
@@ -0,0 +1,59 @@
+package com.zdez.coder.feature_user_list.presentation.loading
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.compose.AppTheme
+import com.zdez.coder.di.AppModule
+import com.zdez.coder.feature_user_list.presentation.MainActivity
+import com.zdez.coder.feature_user_list.presentation.util.Screen
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+@UninstallModules(AppModule::class)
+class LoadingScreenTest{
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @ExperimentalAnimationApi
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+        composeRule.setContent {
+            val navController = rememberNavController()
+            AppTheme {
+                NavHost(
+                    navController = navController,
+                    startDestination = Screen.Loading.route
+                ) {
+                    composable(route = Screen.Loading.route) {
+                        LoadingScreen(navController = navController)
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun clickToggleOrderSection_isVisible() {
+        composeRule.onNodeWithTag(TestTags.ORDER_SECTION).assertDoesNotExist()
+        composeRule.onNodeWithContentDescription("Sort").performClick()
+        composeRule.onNodeWithTag(TestTags.ORDER_SECTION).assertIsDisplayed()
+    }
+}
